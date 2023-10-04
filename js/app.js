810368747662e846edc0a55c8c85dbad3a148e8b
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-10-04 14:59:23 +0300

    added droplets to landing page

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var App = {
+    numberOfDroplets: 100   
+}
+
+App.spawnDroplet = function(input) {
+
+    $('body').append('<div class="droplet"><div class="flying"></div></div>')
+
+    var $droplet = $('body .droplet').last()
+    var $flying = $droplet.find('.flying')
+    $droplet.css({
+        left: input.left + '%',
+        top: input.top + '%',
+        transform: 'scale(' + input.scale + ')',
+        opacity: input.opacity
+    })
+    $flying.css({
+        animation: input.duration + 's flying infinite linear',
+        'animation-delay': input.delay + 's',
+    })
+}
+    
+var i = 0
+while( i < App.numberOfDroplets ) {
+    
+    var opacity = Math.random().toFixed(1)
+    var scale = opacity * 3
+    var y = 65 + ( opacity * 35 )
+    var duration = 1 + parseFloat(Math.random().toFixed(2))
+    
+    App.spawnDroplet({
+        left: parseInt( Math.random() * 100 ),
+        top: y,
+        opacity: opacity,
+        scale: scale,
+        duration: duration,
+        delay: duration
+    })
+    i++
+}    
